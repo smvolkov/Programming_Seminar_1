ff0c8263074497a0d153e8918be885f97ebd588d
smvolkov <121539364+smvolkov@users.noreply.github.com>
2022-12-30 21:12:42 +0500

    Delete Program.cs

--- Program.cs
+++
@@ -1,9 +0,0 @@
-﻿void NumberSquare(double number)
-{
-    Console.WriteLine(number * number);
-}
-
-Console.WriteLine("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-
-NumberSquare(num);
