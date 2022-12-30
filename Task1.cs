dc42bc6d4a7100fb050b6d7465b86f266460f91f
Semion Volkov <semen_volkov@mail.ru>
2022-12-30 22:06:08 +0500

    Task4 added

--- Task1.cs
+++ Task1.cs
@@ -54,12 +54,29 @@
 // -3 -> нет
 // 7 -> нет
 
-void EvenOdd (int value)
+// void EvenOdd (int value)
+// {
+//     if (value % 2 == 0) Console.WriteLine("Да");
+//     else Console.WriteLine("Нет");
+// }
+
+// Console.WriteLine("Введите число: ");
+// int number = int.Parse(Console.ReadLine());
+// EvenOdd(number);
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+void PrintEvenNumbers (int n)
 {
-    if (value % 2 == 0) Console.WriteLine("Да");
-    else Console.WriteLine("Нет");
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 ==0) Console.Write($"{i}, ");
+    }
 }
 
 Console.WriteLine("Введите число: ");
 int number = int.Parse(Console.ReadLine());
-EvenOdd(number);
+PrintEvenNumbers(number);
