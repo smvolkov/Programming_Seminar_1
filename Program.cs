7d24a02f2377e149a3cfcf4c9616d72115fd4ae7
Semion Volkov <semen_volkov@mail.ru>
2022-12-30 20:52:59 +0500

    .gitignore added

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿void NumberSquare(double number)
+{
+    Console.WriteLine(number * number);
+}
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+NumberSquare(num);
