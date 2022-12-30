742f0448988bc32ff8747f5fd89b5046ccac656e
Semion Volkov <semen_volkov@mail.ru>
2022-12-30 21:57:25 +0500

    Task3 added

--- Task1.cs
+++ Task1.cs
@@ -29,22 +29,37 @@
 //44 5 78 -> 78
 //22 3 9 -> 22
 
-void MaxNumberOf3(int number1, int number2, int number3)
-{
-    int max = number1;
+// void MaxNumberOf3(int number1, int number2, int number3)
+// {
+//     int max = number1;
     
-    if (number2 > max) max = number2;
-    if (number3 > max) max = number3;
+//     if (number2 > max) max = number2;
+//     if (number3 > max) max = number3;
 
-    Console.WriteLine($"Max = {max}");
+//     Console.WriteLine($"Max = {max}");
+// }
+
+// Console.WriteLine("Введите первое число: ");
+// int num1 = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int num2 = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите третье число: ");
+// int num3 = int.Parse(Console.ReadLine());
+
+// MaxNumberOf3(num1, num2, num3);
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+void EvenOdd (int value)
+{
+    if (value % 2 == 0) Console.WriteLine("Да");
+    else Console.WriteLine("Нет");
 }
 
-Console.WriteLine("Введите первое число: ");
-int num1 = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
-int num2 = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите третье число: ");
-int num3 = int.Parse(Console.ReadLine());
-
-MaxNumberOf3(num1, num2, num3);
-
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+EvenOdd(number);
