d6906a15111bcb99c322ba5bc56555892d86aecf
Semion Volkov <semen_volkov@mail.ru>
2022-12-30 21:50:22 +0500

    Task2 added

--- Task1.cs
+++ Task1.cs
@@ -4,21 +4,47 @@
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
 
-void MaxNumber(int number1, int number2)
+// void MaxNumber(int number1, int number2)
+// {
+//     if (number1 > number2)
+//     {
+//         Console.WriteLine($"Max = {number1}");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"Max = {number2}");
+//     }
+// }
+
+// Console.WriteLine("Введите первое число: ");
+// int num1 = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int num2 = int.Parse(Console.ReadLine());
+
+// MaxNumber(num1, num2);
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+void MaxNumberOf3(int number1, int number2, int number3)
 {
-    if (number1 > number2)
-    {
-        Console.WriteLine($"Max = {number1}");
-    }
-    else
-    {
-        Console.WriteLine($"Max = {number2}");
-    }
+    int max = number1;
+    
+    if (number2 > max) max = number2;
+    if (number3 > max) max = number3;
+
+    Console.WriteLine($"Max = {max}");
 }
 
 Console.WriteLine("Введите первое число: ");
 int num1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int num3 = int.Parse(Console.ReadLine());
 
-MaxNumber(num1, num2);
+MaxNumberOf3(num1, num2, num3);
+
